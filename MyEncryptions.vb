6f4129145c96099da8e8094e4cecfa5f016b1369
enmorphe <luisbernardmesias@gmail.com>
2017-04-20 13:01:56 +0800

    Encryption Libraries for VB.net

---
+++ MyEncryptions.vb
@@ -0,0 +1,166 @@
+﻿Namespace Encryption
+    Public Class MyEncryptions
+#Region "1"
+        ''' <summary>
+        ''' A simple obscuring of a string
+        ''' Primarily used in my first serial generator
+        ''' </summary>
+        ''' <param name="strToEncode"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Shared Function Encoder1(ByVal strToEncode As String)
+            strToEncode = strToEncode.ToUpper 'always upper case
+            Dim sNumberEquivalence As New Dictionary(Of String, String)
+            Dim sLetterEquivalence As New Dictionary(Of String, String)
+            With sNumberEquivalence
+                .Add("0", "Q") '0
+                .Add("1", "R") '1
+                .Add("2", "S") '2
+                .Add("3", "T") '3
+                .Add("4", "U") '4
+                .Add("5", "V") '5
+                .Add("6", "W") '6
+                .Add("7", "X") '7
+                .Add("8", "Y") '8
+                .Add("9", "Z") '9
+            End With
+            With sLetterEquivalence
+                .Add("Q", "9") 'Q
+                .Add("R", "8") 'R
+                .Add("S", "7") 'S
+                .Add("T", "6") 'T
+                .Add("U", "5") 'U
+                .Add("V", "4") 'V
+                .Add("W", "3") 'W
+                .Add("X", "2") 'X
+                .Add("Y", "1") 'Y
+                .Add("Z", "0") 'Z
+                'Normal Letters
+                .Add("A", "H") 'A
+                .Add("B", "I") 'B
+                .Add("C", "J") 'C
+                .Add("D", "K") 'D
+                .Add("E", "A") 'E
+                .Add("F", "B") 'F
+                .Add("G", "C") 'G
+                .Add("H", "D") 'H
+                .Add("I", "L") 'I
+                .Add("J", "M") 'J
+                .Add("K", "N") 'K
+                .Add("L", "P") 'L
+                .Add("M", "E") 'M
+                .Add("N", "F") 'N
+                .Add("O", "G") 'O
+                .Add("P", "O") 'P
+            End With
+            Dim x As New System.Collections.Generic.List(Of Char)
+            x = strToEncode.ToList()
+            Dim encodedSerial As String = ""
+            For Each a As Char In x
+                If a = "-" Then
+                    encodedSerial = encodedSerial & a
+                Else
+                    'process
+                    If IsNumeric(a) Then
+                        'based on the equivalent of numbers
+                        encodedSerial = encodedSerial & sNumberEquivalence(a)
+                    Else
+                        'the dictionary may not contain the string
+                        'check first
+                        If sLetterEquivalence.ContainsKey(a) Then
+                            encodedSerial = encodedSerial & sLetterEquivalence(a)
+                        Else
+                            'discard that
+                        End If
+                    End If
+                End If
+            Next
+            Return encodedSerial
+        End Function
+        Public Shared Function Decoder1(ByVal encodedSerial As String) As String
+            'REVERSE, the keys and values
+            encodedSerial = encodedSerial.ToUpper 'to be safe
+            Dim sNumberEquivalence As New Dictionary(Of String, String)
+            Dim sLetterEquivalence As New Dictionary(Of String, String)
+            With sNumberEquivalence
+                .Add("Q", "0") '0
+                .Add("R", "1") '1
+                .Add("S", "2") '2
+                .Add("T", "3") '3
+                .Add("U", "4") '4
+                .Add("V", "5") '5
+                .Add("W", "6") '6
+                .Add("X", "7") '7
+                .Add("Y", "8") '8
+                .Add("Z", "9") '9
+            End With
+            With sLetterEquivalence
+                .Add("9", "Q") 'Q
+                .Add("8", "R") 'R
+                .Add("7", "S") 'S
+                .Add("6", "T") 'T
+                .Add("5", "U") 'U
+                .Add("4", "V") 'V
+                .Add("3", "W") 'W
+                .Add("2", "X") 'X
+                .Add("1", "Y") 'Y
+                .Add("0", "Z") 'Z
+                'Normal Letters
+                .Add("H", "A") 'A
+                .Add("I", "B") 'B
+                .Add("J", "C") 'C
+                .Add("K", "D") 'D
+                .Add("A", "E") 'E
+                .Add("B", "F") 'F
+                .Add("C", "G") 'G
+                .Add("D", "H") 'H
+                .Add("L", "I") 'I
+                .Add("M", "J") 'J
+                .Add("N", "K") 'K
+                .Add("P", "L") 'L
+                .Add("E", "M") 'M
+                .Add("F", "N") 'N
+                .Add("G", "O") 'O
+                .Add("O", "P") 'P
+            End With
+            Dim x As New System.Collections.Generic.List(Of Char)
+            x = encodedSerial.ToList()
+            Dim decodedSerial As String = ""
+            Dim numericEquivString As String = "QRSTUVWXYZ"
+            For Each a As Char In x
+                If a = "-" Then
+                    decodedSerial = decodedSerial & a
+                Else
+                    'process
+                    If InStr(numericEquivString, a.ToString, CompareMethod.Binary) Then
+                        'based on the equivalent of numbers
+                        decodedSerial = decodedSerial & sNumberEquivalence(a)
+                    Else
+                        If sLetterEquivalence.ContainsKey(a) Then
+                            decodedSerial = decodedSerial & sLetterEquivalence(a)
+                        End If
+                    End If
+                End If
+            Next
+            Return decodedSerial
+        End Function
+#End Region
+#Region "Random Strings"
+        Public Shared Function GenerateRandomString(ByVal strLength As Integer) As String
+            Dim xCharArray() As Char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray
+            Dim xNoArray() As Char = "0123456789".ToCharArray
+            Dim xGenerator As System.Random = New System.Random()
+            Dim xStr As String = String.Empty
+            While xStr.Length < strLength
+                If xGenerator.Next(0, 2) = 0 Then
+                    xStr &= xCharArray(xGenerator.Next(0, xCharArray.Length))
+                Else
+                    xStr &= xNoArray(xGenerator.Next(0, xNoArray.Length))
+                End If
+            End While
+            Return xStr
+        End Function
+#End Region
+        
+    End Class
+End Namespace
---
+++ TelerikBase64Convert.vb
@@ -0,0 +1,67 @@
+﻿Namespace Encryption
+    Public Class TelerikBase64Convert
+        Private Shared Function BaseConvert(ByVal number As String, ByVal fromBase As Integer, ByVal toBase As Integer) As String
+            Dim digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            Dim length = number.Length
+            Dim result = String.Empty
+
+            Dim nibbles = number.[Select](Function(c) digits.IndexOf(c)).ToList()
+            Dim newlen As Integer
+            Do
+                Dim value = 0
+                newlen = 0
+
+                For i As Double = 0 To length - 1
+                    value = value * fromBase + nibbles(i)
+                    If value >= toBase Then
+                        If newlen = nibbles.Count Then
+                            nibbles.Add(0)
+                        End If
+                        nibbles(System.Math.Max(System.Threading.Interlocked.Increment(newlen), newlen - 1)) = value / toBase
+                        value = value Mod toBase
+                    ElseIf newlen > 0 Then
+                        If newlen = nibbles.Count Then
+                            nibbles.Add(0)
+                        End If
+                        nibbles(System.Math.Max(System.Threading.Interlocked.Increment(newlen), newlen - 1)) = 0
+                    End If
+                Next
+                length = newlen
+                '
+                result = digits(value) + result
+            Loop While newlen <> 0
+
+            Return result
+        End Function
+        '=======================================================
+        'Service provided by Telerik (www.telerik.com)
+        'Conversion powered by NRefactory.
+        'Twitter: @telerik
+        'Facebook: facebook.com/telerik
+        '=======================================================
+        Public Shared Function HashValueAndClean(ByVal value As String) As String
+            If value.Trim = "" Then Return "" 'do not allow empty string
+            value = value.ToUpper
+            Dim hashed As String
+            Dim WantedChars As String = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789" 'i did not include 0 and the letter O to prevent confusion :)
+            hashed = BaseConvert(value, 16, 36) 'use this one because it always return the same code
+            hashed = hashed.ToUpper() 'capitalize
+
+            Dim clean_string As String = "", x As Integer
+            Dim examine_str As String
+            'may include unwanted characters, remove it
+            For x = 1 To hashed.Length
+                examine_str = Mid(hashed, x, 1)
+                If Strings.InStr(WantedChars, examine_str, CompareMethod.Text) Then
+                    'try its in there
+                    clean_string = clean_string & examine_str
+                End If
+            Next
+
+            Return clean_string
+
+        End Function
+    End Class
+End Namespace
+
+
